e1f9500dc69423433c328a02847ed67402abbffb
LachlanCourt <80299053+LachlanCourt@users.noreply.github.com>
2021-10-16 13:44:03 +1100

    Rsa keygen (#3)
    * Add prime generator
    
    * Add powmod function
    
    * Add keygen
    
    * Tidy up output
    
    * Add ID exchange

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import socket
+from IdGen import IdGen
 
 def log(msg):
     print("CLIENT LOGGING: " + msg)
 
+def serialise(d):
+    return str.encode(str(d))
+
 if __name__ == "__main__":
+    idGen = IdGen()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Connect to server
         log("Connecting to server")
@@ -11,7 +16,27 @@ if __name__ == "__main__":
         log("Connected")
         
         # Send setup request
-        log("Sending setup request")
-        s.sendall(b"hello")
+        log("Sending setup request: Hello")
+        s.sendall(b"Hello")
 
+        # Receive keys
+        data = s.recv(2048)
+        rsan = data
+        log(f"Key n received: \n{rsan}")
+        data = s.recv(2048)
+        rsae = data
+        log(f"Key e received: \n{rsae}")
+
+        # Send Client Hello
+        idc = idGen.getID(20)
+        log(f"Sending client hello:\n {idc}")
+        s.sendall(serialise(idc))
+
+        # Receive IDs and SID
+        data = s.recv(2048)
+        ids = data
+        log(f"IDs received: \n{ids}")
+        data = s.recv(2048)
+        sid = data
+        log(f"SID received: \n{sid}")
         
