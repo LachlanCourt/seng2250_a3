acae9ac16eb74ef62885446b4b7f6536fee490a1
LachlanCourt <c3308061@uon.edu.au>
2021-10-16 11:13:12 +1100

    Add powmod function

--- server.py
+++ server.py
@@ -3,6 +3,18 @@ import socket
 def log(msg):
     print("SERVER LOGGING: " + msg)
 
+
+def powmod2(b, e, n):
+    if n == 1:
+        return 0
+    rs = 1
+    while (e > 0):
+        if (e & 1) == 1:
+            rs = (rs * b) % n
+        e = e >> 1
+        b = (b * b) % n
+    return rs
+
 PORT = 50007
 
 if __name__ == "__main__":
