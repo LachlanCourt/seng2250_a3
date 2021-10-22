52a8a798dfb1126b2818831663305f8fdd69f1ba
LachlanCourt <c3308061@uon.edu.au>
2021-10-22 12:00:39 +1100

    Add data exchange

--- client.py
+++ client.py
@@ -5,12 +5,13 @@ from SHA import SHA
 from Comms import Comms
 from DH import DH
 from Hmac import Hmac
+from AES2 import AES2
 
 DHp = 178011905478542266528237562450159990145232156369120674273274450314442865788737020770612695252123463079567156784778466449970650770920727857050009668388144034129745221171818506047231150039301079959358067395348717066319802262019714966524135060945913707594956514672855690606794135837542707371727429551343320695239
 DHg = 174068207532402095185811980123523436538604490794561350978495831040599953488455823147851597408940950725307797094915759492368300574252438761037084473467180148876118103083043754985190983472601550494691329488083395492313850000361646482644608492304078721818959999056496097769368017749273708962006689187956744210730
 
 def log(msg):
-    print("CLIENT LOGGING: " + msg)
+    print("\nCLIENT LOGGING: " + msg)
 
 def serialise(data):
     return str.encode(str(data))
@@ -100,12 +101,30 @@ if __name__ == "__main__":
             s.close()
             sys.exit(1)
 
-        
+        ###### Data Exchange ######
+
+        # Send message
         # Create HMAC from session key
         message = "This message is exactly 64 bytes long to be encrypted with AES!!"
-        messageMac = Hmac.hmac(message, sessionKey)
-        s.sendall(serialise(message + "#" + messageMac))
-        log(f"Sending session key: \n{message}")
+        encryptedMessage = AES2.encrypt(message, sessionKey)
+        #encryptedMessage = message
+        log(f"Generated encrypted message: \n{encryptedMessage}")
+        messageMac = Hmac.hmac(encryptedMessage, sessionKey)
+        log(f"Hashed message for integrity: \n{messageMac}")
+        sendMessage = encryptedMessage + "#" + messageMac
+        s.sendall(serialise(sendMessage))
+        log(f"Sending encrypted message: \n{message}")
+
+
+        # Receive message
+        data = deserialise(s.recv(10000)).split("#")
+        log(f"Received AES encrypted message with hash verification: \n{data}")
+        if not Hmac.verHmac(data[0], data[1], sessionKey):
+            s.close()
+            sys.exit(1)
+        log(f"Hash verified data: \n{data[0]}")
+        message = AES2.decrypt(data[0], sessionKey)
+        log(f"Decrypted AES data to original message: \n{message}")
 
 
 
